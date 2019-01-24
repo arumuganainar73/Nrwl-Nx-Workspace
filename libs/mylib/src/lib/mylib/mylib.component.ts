0a848b4c98647f7b20200c005cdd45de3e968636
Arumuganainar <arumuganainar73@gmail.com>
2019-01-24 10:41:25 +0530

    initial commit

---
+++ libs/mylib/src/lib/mylib/mylib.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mylib-mylib',
+  templateUrl: './mylib.component.html',
+  styleUrls: ['./mylib.component.scss']
+})
+export class MylibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
